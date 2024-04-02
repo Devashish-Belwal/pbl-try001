dd06be1c07f778fb23cb18f3c346a9dc9ea4355c
Devashish-Belwal <131748667+Devashish-Belwal@users.noreply.github.com>
2024-04-03 05:12:41 +0530

    Added Team Standings

--- pbl-site-frontend-001/src/App.js
+++ pbl-site-frontend-001/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Navbar from './component/Navbar.js';
 import HomeCarousel from './component/HomeCarousel.js';
+import TeamStandings from './component/TeamStandings.js';
 
 function App() {
-  return (<div>
+  return (<div className='mainBackground'>
     <Navbar/>
     <HomeCarousel/>
+    <TeamStandings/>
   </div>);
 }
 
--- pbl-site-frontend-001/src/component/HomeCarousel.js
+++ pbl-site-frontend-001/src/component/HomeCarousel.js
@@ -9,21 +9,21 @@ const HomeCarousel = () => {
     return (<center>
         <Carousel>
             <Carousel.Item>
-                <img src={img1} className='CImage d-block' text="First slide" />
+                <img src={img1} className='CImage d-block' alt="First slide" />
                 <Carousel.Caption>
                     <h3>First slide label</h3>
                     <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
-                <img src={img2} className='CImage d-block' text="Second slide" />
+                <img src={img2} className='CImage d-block' alt="Second slide" />
                 <Carousel.Caption>
                     <h3>Second slide label</h3>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
-                <img src={img3} className='CImage d-block' text="Third slide" />
+                <img src={img3} className='CImage d-block' alt="Third slide" />
                 <Carousel.Caption>
                     <h3>Third slide label</h3>
                     <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
